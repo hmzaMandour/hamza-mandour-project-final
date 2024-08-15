5f3235587992d7d4611851fcb48bfce55c90c2f6
hmzaMandour <hamza.mondour12@gmail.com>
2024-08-15 14:45:29 +0100

    section6

--- public/js/app.js
+++ public/js/app.js
@@ -43,32 +43,104 @@ let lunch = document.querySelector("#lunch")
 let dinner = document.querySelector("#dinner")
 
 
+
+
 starters.addEventListener("click",()=>{
-    let a = document.querySelector(".smalldiv")
+    let a = document.querySelector(".title")
     let b = document.querySelector(".topdiv")
-    a.children[1].textContent = "Starters"
+    a.textContent = "Starters"
+    b.style.animation = "up-down 2s ease-in-out  alternate-reverse both"
    
 
 })
 breakfast.addEventListener("click",()=>{
-    let a = document.querySelector(".smalldiv")
+    let a = document.querySelector(".title")
     let b = document.querySelector(".topdiv")
-    a.children[1].textContent = "Breakfast"
-    b.style.background = "red"
+    a.textContent = "Breakfast"
+    b.style.animation = "up-down 2s ease-in-out  alternate-reverse both"
+
    
    
 })
 dinner.addEventListener("click",()=>{
-    let a = document.querySelector(".smalldiv")
+    let a = document.querySelector(".title")
     let b = document.querySelector(".topdiv")
-    a.children[1].textContent = "Lunch"
+    a.textContent = "Dinner"
+    b.style.animation = "up-down 2s ease-in-out  alternate-reverse both"
+
+  
 
    
 })
 lunch.addEventListener("click",()=>{
-    let a = document.querySelector(".smalldiv")
+    let a = document.querySelector(".title")
     let b = document.querySelector(".topdiv")
-    a.children[1].textContent = "Dinner"
+    a.textContent = "Lunch"
+    b.style.animation = "up-down 2s ease-in-out  alternate-reverse both"
+
 
  
 })
+
+
+
+let nextBtns = document.querySelectorAll(".next")
+let containers = document.querySelectorAll(".carousel-container")
+
+
+
+let currentIndex = 0
+const slideImage = (index, myBtn) => {
+
+    let carouselBtnAttribute = myBtn.getAttribute("carouselBtn")
+
+    containers.forEach(container => {
+        if (container.id == carouselBtnAttribute) {
+
+            let slides = container.querySelectorAll(".slide")
+            let camera = container.querySelector(".carousel")
+            let slideWidth = slides[0].clientWidth
+            let indicators = container.querySelectorAll('.indicator')
+            if (index < 0) {
+                index = slides.length - 1
+            } else if (index >= slides.length) {
+                index = 0
+            }
+
+            indicators.forEach(indicator => {
+                indicator.classList.remove('activeIndicator')
+            });
+            indicators[index].classList.add("activeIndicator")
+            camera.style.transform = `translateX(-${slideWidth * index}px)`
+            currentIndex = index
+        }
+    });
+
+
+}
+
+nextBtns.forEach(next => {
+    next.addEventListener("click", (event) => { slideImage(currentIndex + 1, event.target) })
+    setInterval(() => {
+        next.click()
+    }, 3000);
+});
+// 
+
+
+
+
+
+containers.forEach(container => {
+    let slides = container.querySelectorAll(".slide")
+    let indicatorsGrp = document.createElement("div")
+    indicatorsGrp.classList.add("indicators-grp")
+    container.appendChild(indicatorsGrp)
+
+    slides.forEach(slide => {
+        let indicator = document.createElement("div")
+        indicator.classList.add("indicator")
+        indicatorsGrp.appendChild(indicator)
+    });
+    indicatorsGrp.querySelector(".indicator").classList.add('activeIndicator')
+});
